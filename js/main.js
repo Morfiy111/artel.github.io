3ea5bd0b9d1fa3c77950ddea9f1dfc935035be93
morfiy <keks46@yandex.ru>
2025-06-03 10:40:18 +0300

    fix

--- js/main.js
+++ js/main.js
@@ -14,3 +14,21 @@ $(document).ready(function () {
     });
 });
 
+var owl = $('.owl-carousel');
+owl.owlCarousel({
+    items: 1,
+    loop: true,
+    margin: 0,
+    nav: true,
+    dots: true,
+    autoplay: true,
+    autoplayTimeout: 4000,
+    smartSpeed: 700,
+    autoplayHoverPause: true
+});
+$('.play').on('click', function () {
+    owl.trigger('autoplay.play.owl', [4000])
+})
+$('.stop').on('click', function () {
+    owl.trigger('autoplay.stop.owl')
+})
