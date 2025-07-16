438b54c2116c15314bd82e3d745c26bdb9c01098
morfiy <keks46@yandex.ru>
2025-07-16 09:28:23 +0300

    fix

--- js/main.js
+++ js/main.js
@@ -32,3 +32,34 @@ $('.play').on('click', function () {
 $('.stop').on('click', function () {
     owl.trigger('autoplay.stop.owl')
 })
+
+var itemsCount = 4;
+if (window.matchMedia("(max-width: 991px)").matches) {
+    itemsCount = 3;
+}
+if (window.matchMedia("(max-width: 767px)").matches) {
+    itemsCount = 2;
+}
+var owl = $('.owl-carousel-photo');
+owl.owlCarousel({
+    items: itemsCount,
+    loop: true,
+    margin: 10,
+    nav: true,
+    dots: true,
+    autoplay: true,
+    autoplayTimeout: 4000,
+    smartSpeed: 700,
+    autoplayHoverPause: true
+});
+$('.play').on('click', function () {
+    owl.trigger('autoplay.play.owl', [4000])
+})
+$('.stop').on('click', function () {
+    owl.trigger('autoplay.stop.owl')
+})
+
+document.querySelector('.text-center-small .btn-main-white').onclick = () => {
+    document.querySelector('.cookie').style.display = 'none';
+    document.querySelector('.cookie').classList.remove('is-active');
+}
